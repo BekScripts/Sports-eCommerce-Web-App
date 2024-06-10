e9996e34a868b8f4edd47604a6977c4156274084
azizu <azizu@AZIZHP>
2024-06-10 12:50:56 -0400

    Add project files.

---
+++ Sports-eCommerce-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sports_eCommerce_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
